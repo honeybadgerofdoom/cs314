7620a9799f2d8506183583c57dd00623b7ed78b7
Matt Young <asterix@rams.colostate.edu>
2021-01-20 16:45:39 -0700

    with a new method for printing a string

--- GitHubExperiment/src/Experiments.java
+++ GitHubExperiment/src/Experiments.java
@@ -4,6 +4,10 @@ public class Experiments {
         return a*a;
     }
 
+    public static String oHai() {
+        return "oHai";
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Testing another print statement.");
@@ -11,5 +15,6 @@ public class Experiments {
         int x = 2+2;
         System.out.println("Two plus two = " + x);
         System.out.println("This should be 9: " + powers(3));
+        System.out.println(oHai());
     }
 }
