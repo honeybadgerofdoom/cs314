e5698e401dff78c63846c39b7071041fcbcfd963
Matt Young <asterix@rams.colostate.edu>
2021-01-20 14:20:49 -0700

    print statement with math

--- GitHubExperiment/src/Experiments.java
+++ GitHubExperiment/src/Experiments.java
@@ -3,5 +3,7 @@ public class Experiments {
         System.out.println("Hello World");
         System.out.println("Testing another print statement.");
         System.out.println("This is the third print statement.");
+        int x = 2+2;
+        System.out.println("Two plus two = " + x);
     }
 }
