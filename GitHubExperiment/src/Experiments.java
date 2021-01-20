e3d7cfd6940805d204a099c50f45bd05a11c3011
Matt Young <asterix@rams.colostate.edu>
2021-01-20 14:05:07 -0700

    add another print statement

--- GitHubExperiment/src/Experiments.java
+++ GitHubExperiment/src/Experiments.java
@@ -1,5 +1,6 @@
 public class Experiments {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Testing another print statement.");
     }
 }
