beefd45abe701c0855ad9e143178fe3f8db13731
Matt Young <honeybadgerofdoom@gmail.com>
2021-01-23 12:41:18 -0700

    Add Matt's name with an extra z.

--- GitHubExperiment/src/Experiments.java
+++ GitHubExperiment/src/Experiments.java
@@ -22,7 +22,7 @@ public class Experiments {
 
     public static String mattName() {
         // Change this to return your name!
-        return "Shplorgz";
+        return "Shplorgzz";
     }
 
     public static void main(String[] args) {
